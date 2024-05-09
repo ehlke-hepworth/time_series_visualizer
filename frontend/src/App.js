dc0b3d1506b02c8bfecc49ce606e22a6b6991a90
ehlke-hepworth <157199273+ehlke-hepworth@users.noreply.github.com>
2024-05-09 18:24:26 +0200

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Buttons from './components/Buttons';
+import Line from './components/LineFoo1';
+import Line2 from './components/LineFoo2';
+import Line3 from './components/LineFoo3';
+import Line4 from './components/LineFoo4';
+import LineB1 from './components/LineBar1';
+import LineB2 from './components/LineBar2';
+import LineB3 from './components/LineBar3';
+import LineB4 from './components/LineBar4';
+import dataProphetImage from './03-DataProphet-SquareLogo.jpg'; // Make sure the path is correct
+
+function App() {
+  const [deviceData, setDeviceData] = useState([]);
+  
+
+  const fetchData = async () => {
+    try {
+        const response = await fetch('http://localhost:8000/data');
+        const data = await response.json();
+        setDeviceData(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <Router>
+      <div className="App" style={{
+          minHeight: '100vh',
+          width: '100vw',
+          position: 'relative',
+          zIndex: 1
+      }}>
+        <img src={dataProphetImage} alt="Data Prophet Logo" style={{
+          width: '6%',
+          height: 'auto',
+          position: 'fixed', 
+          top: '7%', 
+          left: '91%',
+          zIndex: 2 
+        }} />
+        <Routes>
+        <Route path="/" element={
+          <div className="App" style={{ minHeight: '100vh', width: '100vw', position: 'absolute', zIndex: 1 }}>
+            <Header />
+            <Buttons fetchData={fetchData} />
+          </div>
+        } />
+          <Route path="/" element={<Buttons fetchData={fetchData} />} />
+          <Route path="/device/foo/datasourceA" element={<Line data={deviceData} />} />
+          <Route path="/device/foo/datasourceB" element={<Line2 data={deviceData} />} />
+          <Route path="/device/foo/datasourceC" element={<Line3 data={deviceData} />} />
+          <Route path="/device/foo/datasourceD" element={<Line4 data={deviceData} />} />
+          <Route path="/device/bar/datasourceA" element={<LineB1 data={deviceData} />} />
+          <Route path="/device/bar/datasourceB" element={<LineB2 data={deviceData} />} />
+          <Route path="/device/bar/datasourceC" element={<LineB3 data={deviceData} />} />
+          <Route path="/device/bar/datasourceD" element={<LineB4 data={deviceData} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/LineFoo1.js
@@ -0,0 +1,159 @@
+import React from 'react';
+import { Line as LineChart } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, TimeScale } from 'chart.js';
+import 'chartjs-adapter-date-fns';
+import { useNavigate } from 'react-router-dom';
+import './Line.css';
+
+// Register the components
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  TimeScale
+);
+
+function Line({ data }) {
+  const removeDuplicates = (data) => {
+    const seen = new Set();
+    return data.filter(item => {
+      const key = `${item.timestamp}-${item.value}`; // Create a unique key by combining timestamp and text value
+      if (seen.has(key)) {
+        return false;
+      } else {
+        seen.add(key);
+        return true;
+      }
+    });
+  };
+  const sortedData = data.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
+  const uniqueData = removeDuplicates(sortedData, 'value');
+
+  const navigate = useNavigate();
+
+  // Filter out invalid data and extreme outliers
+  const validData = uniqueData.filter(item => 
+    item.device_name === "device.foo" &&
+    item.parameter === 'tenantA.deviceFoo.datasourceA.bar' &&
+    item.timestamp &&
+    !isNaN(new Date(item.timestamp).getTime()) &&
+    item.value &&
+    !isNaN(parseFloat(item.value)) &&
+    parseFloat(item.value) < 1000 // Example threshold for outliers
+  );
+
+    // Filter data for specific parameter and type 'text'
+  const textData = uniqueData.filter(item => 
+    item.device_name === "device.foo" &&
+    item.parameter === 'tenantA.deviceFoo.datasourceA.foo' &&
+    item.type === 'text' &&
+    item.timestamp &&
+    !isNaN(new Date(item.timestamp).getTime())
+  );
+
+  // Calculate moving average for smoothing
+  const windowSize = 15; // Size of the moving average window
+  const smoothedData = validData.map((item, index, arr) => {
+    const window = arr.slice(Math.max(0, index - windowSize + 1), index + 1);
+    const average = window.reduce((sum, curr) => sum + parseFloat(curr.value), 0) / window.length;
+    return {
+      x: new Date(item.timestamp).getTime(),
+      y: average,
+      text: item.type === 'text' ? item.value.toString() : ''
+    };
+  });
+
+  const chartData = {
+    datasets: [
+      {
+        label: 'QUX Time Series',
+        data: validData.map(item => ({
+          x: new Date(item.timestamp).getTime(),
+          y: parseFloat(item.value),
+          text: item.type === 'text' ? item.value.toString() : ''
+        })),
+        fill: false,
+        backgroundColor: 'rgba(7, 55, 108, 0.2)',
+        borderColor: 'rgba(7, 55, 108, 0.3)',
+        borderWidth: 5,
+      },
+      {
+        label: 'Smoothed Line',
+        data: smoothedData,
+        fill: false,
+        borderColor: '#0cc37e',
+        borderWidth: 4,
+        pointRadius: 0
+      }
+    ]
+  };
+
+  const chartOptions = {
+    scales: {
+      x: {
+        type: 'time',
+        time: {
+          unit: 'day',
+          tooltipFormat: 'yyyy-MM-dd HH:mm:ss',
+          displayFormats: {
+            day: 'yyyy-MM-dd'
+          }
+        },
+        title: {
+          display: true,
+          text: 'Time'
+        }
+      },
+      y: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: 'Value'
+        }
+      }
+    },
+    plugins: {
+      legend: {
+        display: true
+      }
+    },
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
+  return (
+    <div>
+      <div className="button-container">
+        <button onClick={() => navigate('/')} className="home-button">Back to Home</button>
+      </div>
+      <h1>Datasource A</h1>
+      <h2>Bar Time Series</h2>
+      <div style={{ height: '420px' }}>
+        <LineChart data={chartData} options={chartOptions} />
+      </div>
+      <table>
+        <thead>
+        <h2>Foo Time Series</h2>
+          <tr>
+            <th>Timestamp</th>
+            <th>Text Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {textData.map((item, index) => (
+            <tr key={index}>
+              <td>{new Date(item.timestamp).toLocaleString()}</td>
+              <td>{item.value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Line;
