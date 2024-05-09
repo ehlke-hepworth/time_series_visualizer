aea245e462e622359b3fa8d9c22af26151d9159c
ehlke-hepworth <157199273+ehlke-hepworth@users.noreply.github.com>
2024-05-09 12:34:44 +0200

    Add files via upload

---
+++ frontend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000'  // Assuming your React frontend is served from this URL
+}));
+
+// Define the route to fetch data
+app.get('/data', async (req, res) => {
+    try {
+        const response = await axios.get('http://localhost:8000/data/');
+        console.log("Data received from FastAPI:", response.data);
+        if (!response.data || !Array.isArray(response.data)) {
+            console.error('Data is not an array or is undefined');
+            res.status(500).json({ error: 'Data is not an array or is undefined' });
+            return;
+        }
+        res.json(response.data);
+    } catch (error) {
+        console.error('Failed to fetch data from FastAPI:', error);
+        res.status(500).send('Failed to fetch data');
+    }
+});
+
+app.listen(8000, () => console.log('Server running on http://localhost:8000'));
